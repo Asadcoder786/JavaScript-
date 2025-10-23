6d9ce945a9eb413403f8474893e9ba3fb0227558
Asadcoder786 <anonymousustad@gmail.com>
2025-10-22 22:58:20 -0700

    My 5th javascript Assigement

---
+++ js5/script.js
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+
+
+let secretNo = 7
+let userGuess = prompt("Guess the secret NO (1 to 10)")
+if (userGuess==7) (
+    alert("Bingo! Correct Answer")
+)
+else (userGuess<7 && userGuess>=8)
+ {
+    alert("You are so close")
+}
