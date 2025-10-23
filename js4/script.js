4115e748e51e30cc4e4c15e1c9d53b7b224e981e
Asadcoder786 <anonymousustad@gmail.com>
2025-10-22 21:08:02 -0700

    This is my 4th javascript Assigement

---
+++ js4/script.js
@@ -0,0 +1,388 @@
+// chapter 9 to 11
+
+// Q1
+
+// let city = prompt("Enter your city name");
+// if (city == "karachi") {
+//     alert("Welcome to The City of Lights")
+// }
+//  Q2
+
+
+// let gender = prompt("Enter Your Gender (male/female):")
+
+
+
+// if (gender == "male") {
+//     alert("Good Morning Sir");
+// }
+// else  {
+//     alert("Good Morning Mam");
+// }
+
+
+// Q3
+
+
+// let colorTrafficLight = prompt("Enter Your Signal Color (red,yellow,green):")
+// if (colorTrafficLight == "red") {
+//     alert("Must Stop")
+// }
+// else if (colorTrafficLight == "yellow") {
+//     alert("Ready to Move")
+// }
+// else if (colorTrafficLight == "green") {
+//     alert("Move Now")
+// }
+
+// Q4 
+
+
+// let carFuel = prompt("Enter the fuel in your car ")
+// if (carFuel<"0.25") {
+//     alert("Please refill the fuel in your car")
+// }
+
+
+
+// Q5 
+
+
+
+// let a = 4;
+// if (++a===5) {
+//     alert("given condition for variable is true")
+// }
+// let b = 82;
+// if (b++===83) {
+//     alert("given condition for variable b is true")
+// }
+// let c = 12;
+// if  ( c++===13) {
+//     alert("condition 1 is true")
+// }
+// if  ( c===13) {
+//     alert("condition 2 is true")
+// }
+// if (++c<14) {
+//     alert("condition 3 is true")
+
+// }
+// if (c === 13 ) {
+//     alert("condition 4 is true")
+// }
+
+// let materialCost = 20000;
+// let laborCost = 2000;
+// let totalCost = materialCost + laborCost;
+// if (totalCost===laborCost+materialCost) {
+//     alert("The costs Equals")
+// }
+// if (true) {
+//     alert("True");
+// }
+// if (false) {
+//     alert("False");
+// }
+// if ("car" < "cat") {
+//     alert("Car is smaller than cat")
+// }
+
+// Q6
+
+
+// let sub1 = Number(prompt("Enter your Math No"))
+// let sub2 = Number(prompt("Enter your English No"))
+// let sub3 = Number(prompt("Enter your Urdu No"))
+// let totalMarks = 300
+// let marksobtained = sub1 + sub2 +sub3
+// document.writeln("Total Marks = " + totalMarks, "<br>")
+// document.writeln("Marks Obtained = " + marksobtained , "<br>")
+// let percentage = (marksobtained/totalMarks ) *100
+// let a = 80
+// let b = 70
+// let c = 60
+// if (percentage>=a ) {
+//     document.writeln(    "percentage= " + percentage , "<br>" )
+//     document.writeln("Grade= A"  , "<br>")
+//     document.writeln("Remarks= Excellent")
+// }
+// else if (percentage>=b ) {
+//     document.writeln(    "percentage= " + percentage , "<br>" )
+//     document.writeln("Grade= B"  , "<br>")
+//     document.writeln("Remarks= You Need To Improvement")
+// }
+// if (percentage>=c ) {
+//     document.writeln(    "percentage= " + percentage , "<br>" )
+//     document.writeln("Grade= c"  , "<br>")
+//     document.writeln("Remarks= One sheet of Paper Can't decide your future")
+// }
+
+// Q7
+
+
+// let secretNo = 7
+// let userGuess = prompt("Guess the secret NO (1 to 10)")
+// if (userGuess==7) (
+//     alert("Bingo! Correct Answer")
+// )
+// else (userGuess<7 && userGuess>=8)
+//  {
+//     alert("You are so close")
+// }
+
+
+// Q8
+
+
+// let userdivnum = +prompt("Enter Your Number to divide by 3 ")
+// if (userdivnum % 3  ===  0) {
+//     alert("Your number is divisible by 3")
+// } else {false} (
+//     alert("Your number is not divisible by ")
+// )
+
+//  Q9
+
+
+
+// var even_odd = prompt("Enter your even or odd number")
+// if (even_odd%2 === 0) {
+//     console.log(even_odd, "This is even Number")
+
+// }else {
+//     console.log(even_odd , "This is odd Number")
+// }
+
+
+
+// Q10 
+
+
+// let temperature = prompt("Enter the Temperature Number")
+// if (temperature >= 40) {
+//     alert("It is too Hot Outside")
+// }else if (temperature >=30 ) {
+//     alert("The weather today is normal")
+// }else if (temperature >=20) {
+//     alert("Today's weather is cool")
+
+// }else if (temperature>=10) {
+//     alert("OMG! Today's weather is soo coll")
+// }
+
+
+
+
+// chaprer 12 to 13
+
+
+// Q1
+
+
+// let A = prompt("Enter you number Upper case")
+// let Z = prompt("Enter you number Upper case")
+// let a = prompt("Enter you number lower case")
+// let z = prompt("Enter you number lower case")
+// if (A=>70) {
+//     alert("Your number is upper case letter A")
+// }else if (Z=>90) {
+//     alert("Your number is upper case letter Z")
+// }else if (a=>97) {
+//     alert("Your number is lower case letter a")
+// }else if (z=>122) {
+//     alert("Your number is lower case letter z")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
