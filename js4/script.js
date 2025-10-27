12e38f09f31e811d98548e38aa1e994c8d918f94
Asadcoder786 <anonymousustad@gmail.com>
2025-10-26 20:42:48 -0700

    My 4th assigement

--- js4/script.js
+++ js4/script.js
@@ -173,6 +173,25 @@
 
 
 
+// Q11
+
+
+// let add1 = +prompt("Enter First number")
+// let add2 = +prompt("Enter Second number")
+// let operator = add1 + add2;
+// let operator2 = add1 - add2;
+// let operator3= add1 * add2;
+// let operator4 = add1 / add2;
+// let operator5 = add1 % add2;
+
+
+// console.log(operator)
+// console.log(operator2)
+// console.log(operator3)
+// console.log(operator4)
+// console.log(operator5)
+
+
 
 // chaprer 12 to 13
 
@@ -193,6 +212,122 @@
 // }else if (z=>122) {
 //     alert("Your number is lower case letter z")
 // }
+ 
+// Q2
+
+
+// let a = +prompt("Enter the  first numbers I will give you the message which one is larger")
+// let b = +prompt("Enter the  second numbers I will give you the message which one is larger")
+// if (a === b ) {
+//     alert("Both are equal")
+// }else if (a > b) {
+//     alert("First one is larger")
+// }else {a < b } (
+//     alert("Second one is larger")
+// )
+
+
+
+// Q3
+
+
+// let d = 0
+// let  c = +prompt("Enter the number I will answer you this is posisitive , negative or zero.")
+
+// if ( c > 0 ) {
+//     alert(" YOur number is positive")
+// } else if ( c < 0 ) {
+//     alert("Number is Negative")
+// }else { c = 0 } (
+//     alert("Your Number is equal to zero")
+// )
+
+
+// Q4
+
+// let  userValue = prompt("Enter you Value")[0];
+
+// let vowel_a = "a"
+// let vowel_e = "e"
+// let vowel_i = "i"
+// let vowel_o = "o"
+// let vowel_u = "u"
+
+
+// if ( userValue == vowel_a) {
+//     alert("This is a vowel")
+// } else if ( userValue == vowel_e) {
+//     alert("This is a vowel")
+// } else if ( userValue == vowel_i) {
+//     alert("This is a vowel")
+// } else if ( userValue == vowel_o) {
+//     alert("This is a vowel")
+// } else if ( userValue == vowel_u) {
+//     alert("This is a vowel")
+// }
+
+
+// Q5
+
+// let userPassword = prompt("Enter your Password")
+// let correctPassword = ("1234")
+
+// if ( userPassword === correctPassword  ) {
+//     alert(" Correct Password")
+// }else if (userPassword && correctPassword) {
+//     alert("Incorrect Password")
+// }else {
+//     alert("Please enter your password")
+// }
+
+
+// Q6
+
+
+// var greeting;
+// var hour = +prompt("Enter the Number")
+// if ( hour < 18) {
+//     console.log("Greeting Good day ")
+// }else (
+//     console.log("Greeting Good Evening")
+// )
+
+// Q7
+
+
+
+// let time = +prompt("The the Time Value Here")
+// if ( time >= 0 && time < 1200)
+// {
+//     console.log("Good Morning")
+// }else if (time >= 1200 && time < 1700 ) {
+//     console.log ("Good Afternoon")
+// }else if (time >= 1700 && time < 2100 ) {
+//     console.log("Good Evening")
+// }else {time >= 2100 && time < 2349} (
+//     console.log("Good Night")
+// )
+
+
+// --------------------------------------------------------Assigement End-----------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
