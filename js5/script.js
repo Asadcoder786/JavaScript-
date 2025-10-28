6f4ce2870a0599381b3aad7746f3975ee3262a6b
Asadcoder786 <anonymousustad@gmail.com>
2025-10-27 21:55:59 -0700

    ATM Machine Logic

--- js5/script.js
+++ js5/script.js
@@ -3,15 +3,75 @@
 
 
 
+alert("Welcome to ATM Machine \nHello user!");
 
+let pinCode = 4527;
 
+let userCode = prompt("Enter your pin")
+if (pinCode == userCode) {
+    console.log("Thk ha pin code paise lane ha")
+    let kitneChiyeHa = prompt("Enter Yes or no")
+    let kiaPaiseLaneHa = "yes";
+    if (kiaPaiseLaneHa == kitneChiyeHa) {
+        console.log("Kitne lane ha");
+    } else {
+        console.log("Phir kya karna aye hu idhar")
+    }
+    let AtmBalance = 50000;
+    let userAmount = prompt("Enter your amount");
+    let userWithdraw = prompt("Withdraw kitne karne ha");
+    if (AtmBalance > userAmount) {
+        console.log("ye amount nikl jaye gi apki", userAmount);
+        if (userAmount > userWithdraw) {
+            userAmount = userAmount - userWithdraw
+            console.log("yE LO PASIE", userAmount);
+        }
+    } else {
+        console.log("Apka Bank ma Itna Paisa Nahi Ha");
 
-let secretNo = 7
-let userGuess = prompt("Guess the secret NO (1 to 10)")
-if (userGuess==7) (
-    alert("Bingo! Correct Answer")
-)
-else (userGuess<7 && userGuess>=8)
- {
-    alert("You are so close")
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
